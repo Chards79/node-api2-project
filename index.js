b4c4c3a6e4b413aad3c2a86d6fe74a099fe245a8
Eric Richards <esholundr@yahoo.com>
2019-12-03 17:02:00 -0700

    created POST and GET all posts endpoints

--- index.js
+++ index.js
@@ -10,7 +10,55 @@ server.get('/', (req, res) => {
     res.send({ api: 'up and running' });
 });
 
+// POST to /api/posts
 
+server.post('/api/posts', (req, res) => {
+    const data = req.body;
+
+    if (data.title && data.contents) {
+        db.insert(data)
+            .then(posts => {
+                res.status(201).json({ ...posts, ...data })
+            })
+            .catch(error => {
+                console.log('error on POST for posts', error);
+                res.status(500).json({ error: 'There was an error while saving the post to the database' })
+            })
+    } else {
+        res.status(400).json({ errorMessage: 'Please provide title and contents for the post' })
+    }
+})
+
+// POST to /api/posts/:id/comments
+
+server.post('/api/posts/:id/comments', (req, res) => {
+
+})
+
+// GET to /api/posts
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(error => {
+            console.log('error on get for all posts', error);
+            res.status(500).json({ error: 'The posts information could not be retrieved' })
+        })
+})
+
+// GET to /api/posts/:id
+
+
+
+// GET to /api/posts/:id/comments
+
+
+// DELETE to /api/posts/:id
+
+
+// PUT to /api/posts/:id
 
 
 
