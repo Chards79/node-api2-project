f177a03ddd9a3acd9669fe576274f30bc5bb4910
Eric Richards <esholundr@yahoo.com>
2019-12-05 17:07:07 -0700

    added dotenv

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const server = require('./api/server');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log(`\n ** API running on port ${port} **\n`)
 });
